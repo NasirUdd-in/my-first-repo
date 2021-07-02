a5092cfa99cbd87d8e743a5fa9c5db778898634c
Nasir Uddin <nasir.diudevs@gmail.com>
2021-07-02 23:55:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+document.write("Row on row");
+
+console.log("p for the p");
+
+document.getElementById("demo").innerHTML = "The quick brown fox jumps over the lazy dog .";
+
+
+document.getEllementById('demo').innerHTML = "The quick brown fox jumps over the lazy dog.";
+
+DocumentFragment.getElementById('demo.innerHTML = "The quick brown fox jumps over the lazy');
+document.write('Row on row');
+
+
